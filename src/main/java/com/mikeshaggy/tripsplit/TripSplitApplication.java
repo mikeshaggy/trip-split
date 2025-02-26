67f2a140b72191f7d9764fbed89ec857933dc67f
mikeshaggy <michalbagan@icloud.com>
2025-02-26 16:17:09 +0100

    init

---
+++ src/main/java/com/mikeshaggy/tripsplit/TripSplitApplication.java
@@ -0,0 +1,13 @@
+package com.mikeshaggy.tripsplit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TripSplitApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TripSplitApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/mikeshaggy/tripsplit/TripSplitApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mikeshaggy.tripsplit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TripSplitApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
